4013f194cfa9bf93aea9e8c76bc3121f409cc229
samisaga95 <santoshkumaramisagadda@Santoshkumars-MacBook-Pro.local>
2019-03-18 02:49:02 -0700

    for production

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const store = configureStore();
 // store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
 // store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
-// console.log(store.getState());
+// console.log (store.getState());
 
 // const state = store.getState();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
