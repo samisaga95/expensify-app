428b5869da567bcbc133c99cc25f1b023e55de73
samisaga95 <santoshkumaramisagadda@Santoshkumars-MacBook-Pro.local>
2019-03-18 23:40:22 -0700

    Formatting for amount and date

--- src/components/expenseListItem.js
+++ src/components/expenseListItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
   return (
@@ -8,9 +10,9 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
       <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
       </Link>
-      <p>
-        {amount} - {createdAt}
-      </p>
+      <p>{numeral(amount / 100).format("$0,0.00")}</p>
+      <br />
+      <p>-{moment(createdAt).format("MMMM Do, YYYY")}</p>
     </div>
   );
 };
