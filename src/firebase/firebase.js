127eaa8c98e4d3324876610d57dcbeb13a648828
samisaga95 <santoshkumaramisagadda@Santoshkumars-MacBook-Pro.local>
2019-04-24 17:22:21 -0700

    hid firebase config details

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
-var config = {
-  apiKey: "AIzaSyD3wPQB4iQ5GfXpVczHTbm8dM7aJB7Vx58",
-  authDomain: "expensify-b95df.firebaseapp.com",
-  databaseURL: "https://expensify-b95df.firebaseio.com",
-  projectId: "expensify-b95df",
-  storageBucket: "expensify-b95df.appspot.com",
-  messagingSenderId: "26545692423"
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
