aa9d50150fb703baaf5061fb373ff3c8bcb6845c
samisaga95 <santoshkumaramisagadda@Santoshkumars-MacBook-Pro.local>
2019-04-24 19:49:37 -0700

    First auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
   </header>
 );
 
